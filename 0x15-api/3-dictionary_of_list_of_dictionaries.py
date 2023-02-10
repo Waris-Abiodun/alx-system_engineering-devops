e7420112672be3edd4bbead48933524a3150c4c1
waris <kazeemwaris05@gmail.com>
2023-02-10 22:19:55 +0100

    getting all the user in json format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    """
+    Main function of the script.
+    Retrieves employee information and to-do list information using the
+    REST API, and prints the to-do list progress for the employee.
+    """
+
+    # Base URL for the API endpoints
+    url = "https://jsonplaceholder.typicode.com/"
+    # Endpoint to get employee info
+    user = requests.get(url + "users").json()
+    # Endpoint to get todo list for the employee
+    todos = requests.get(url + "todos").json()
+
+    # Write the user to-do list information to a JSON file
+    with open("todo_all_employees.json", "w") as JSONFile:
+        # Write the information as a dictionary, with the key as the user ID
+        # and the value as a list of dictionaries, each containing information
+        # about a to-do task
+        for dct in user:
+            data = {dct.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": dct.get("username")
+                 } for t in todos]}
+            json.dump(data, JSONFile)
